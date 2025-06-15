5aab3afe88237296d31f860c936e2aabf09a2eab
IliaKurdukov <79817899982@ya.ru>
2025-06-16 01:11:02 +0300

    Add files via upload

--- hidrodata.py
+++ hidrodata.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import scipy.stats as stats
 
 st.title("📊 Построение кривых обеспеченности")
 
@@ -13,11 +14,11 @@ if uploaded_file:
         #st.write(result['notes'])
         # Автоматическое определение столбцов
         numeric_cols = df.select_dtypes(include=['number']).columns
+        cols = df.columns.tolist()
         if len(numeric_cols) == 0:
             st.error("В файле нет числовых столбцов")
         else:
             values_col = st.selectbox("Выберите столбец с данными для построения кривой обеспеченности", numeric_cols)
-            cols = df.columns.tolist()
             cols.insert(0, 'Без группировки')
             index_col = st.selectbox("Выберите столбец для группировки данных", cols)
             if index_col != 'Без группировки':
@@ -26,12 +27,50 @@ if uploaded_file:
               data = df.pivot_table(index = index_col, values = values_col, aggfunc = aggfunc_dict[aggfunc])
             else:
               data = df[values_col]
+            data.sort_values(by = 'values_col', inplace=True)
+            data = pd.DataFrame(data)
+            data['Ранг'] = range(len(data))
+            data['Вероятность'] = 1 - (data['Ранг'] + 1) / (pivot_df['Ранг'].max() + 1)
+
             distributions = {'Гумбеля': 'gumbel_r', 'Фреше': 'genextreme', 'Пирсона 3 типа': 'pearson3'}
             disribution = st.selectbox("Выберите распределение для аппроксимации", distributions)
+            dist_key = distributions[disribution]
+            selected_dist = getattr(stats, dist_key)  # Получаем класс распределения
 
-            # Гистограмма
+            # Пример вычисления PDF (функции плотности)
+            data_points = np.linspace(-3, 3, 100)
+            pdf_values = selected_dist.pdf(data_points, *params)  # params - параметры распределения
+
+            # инициализация функции для изменения масштаба по горизонтальной оси
+            def scalefunc(x):
+              return stats.norm.ppf(x/100, loc=0, scale=1)
+              
+            # График
             fig, ax = plt.subplots()
-            ax.hist(data)
+            params = selected_dist.fit(data[values_col])
+
+            x = data['Вероятность'] * 100
+            y = data['Высота']
+            plt.scatter(x, y, label='Эмпирическое распределение')
+
+            # построение кривой с распределением
+            def f(x):
+                return selected_dist.ppf(1-x/100, params)
+            f2 = np.vectorize(f)
+            x = np.arange(0.1, 99.9, 0.1)
+            plt.plot(x, f2(x), color = 'red', label='Теоретическое распределение')
+
+            # добавление линий сетки, масштаба по горизонтальной оси, подписей осей и графика,
+            # границ, шага и подписей делений для горизонтальной оси
+            ax.xaxis.grid(True)
+            plt.xscale('function', functions=[scalefunc, lambda x: x])
+            ax.set(xlabel="Обеспеченность, %")
+            ax.set(ylabel="Инализируемая величина")
+            ax.set(title="Значения анализируемой величины с разной долей обеспеченности")
+            ax.set(xlim=(0.1,99.9))
+            plt.xticks([0.1, 1, 2, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 98, 99, 99.9])
+            ax.set_xticklabels([0.1, 1, 2, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 95, 98, 99, 99.9])
+            plt.legend()
             st.pyplot(fig)
 
     except Exception as e:
