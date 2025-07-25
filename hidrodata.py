cfd1f3ac9b684dd72d7d1bd55204fdb01e2d2e2e
Ilia <79817899982@ya.ru>
2025-07-25 23:36:59 +0300

    Add files via upload

--- hidrodata.py
+++ hidrodata.py
@@ -36,8 +36,9 @@ if uploaded_file:
             data['Вероятность'] = 1 - (data['Ранг'] + 1) / (data['Ранг'].max() + 2)
 
             distributions = {'Гумбеля (метод максимального правдоподобия)': 'gumbel_r',
-                             'Фреше (метод максимального правдоподобия)': 'genextreme',
-                             'Пирсона 3 типа (метод максимального правдоподобия)': 'pearson3'}
+                             'Фреше (метод максимального правдоподобия)': 'invweibull',
+                             'Пирсона 3 типа (метод максимального правдоподобия)': 'pearson3',
+                             'Обобщенное (метод максимального правдоподобия)': 'genextreme'}
             disribution = st.selectbox("Выберите распределение для аппроксимации", distributions)
             dist_key = distributions[disribution]
             selected_dist = getattr(stats, dist_key)  # Получаем класс распределения
