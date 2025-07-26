b6e7d16e1c3576cb81a269b2bbc89baacf06d4ca
Ilia <79817899982@ya.ru>
2025-07-26 14:23:00 +0300

    Add files via upload

--- hidrodata.py
+++ hidrodata.py
@@ -12,9 +12,15 @@ uploaded_file = st.file_uploader("Загрузите XLS файл")
 if uploaded_file:
     try:
         df = pd.read_excel(uploaded_file)
-        st.success(f"Данные успешно загружены и содержат {len(df)} строк. \n\n Ниже представлен пример данных:")
-        #st.table(df.sample(3))
-        st.markdown(df.sample(3).to_html(), unsafe_allow_html=True)
+        def pluralize_rows(number: int) -> str:
+          if number % 10 == 1 and number % 100 != 11:
+              return "строку"  # 1 строка (но 11, 111, 211 и т.д. — "строк")
+          elif 2 <= number % 10 <= 4 and (number % 100 < 10 or number % 100 >= 20):
+              return "строки"  # 2, 3, 4 строки (но 12, 13, 14 — "строк")
+          else:
+              return "строк"  # 0, 5-20, 25-30 и т.д.
+        st.success(f"Данные успешно загружены и содержат {len(df)} {pluralize_rows{len(df)}}. Ниже представлен пример данных:")
+        st.markdown(df.head(3).to_html(), unsafe_allow_html=True)
         # Автоматическое определение столбцов
         numeric_cols = df.select_dtypes(include=['number']).columns
         cols = df.columns.tolist()
@@ -64,8 +70,10 @@ if uploaded_file:
                         linewidths=1)    # толщина контура
             
             # таблица
-            percent_list = [0.01, 0.1, 0.5, 1, 2, 3, 5, 10, 50, 90, 95, 98, 99]
-            df = pd.DataFrame(percent_list, columns=['Обеспеченность'])
+            percent_list_1 = [0.01, 0.1, 0,33, 0.5, 1, 2, 3, 5]
+            percent_list_2 = [10, 50, 63, 90, 95, 98, 99, 99.9]
+            df_1 = pd.DataFrame(percent_list_1, columns=['Обеспеченность'])
+            df_2 = pd.DataFrame(percent_list_2, columns=['Обеспеченность'])
             # Функция для форматирования чисел в таблице
             def custom_round(x):
               abs_x = abs(x)
@@ -90,9 +98,8 @@ if uploaded_file:
               plt.plot(x, f2(x), label= f'Распределение {teor_label} ({round(mae, 1)})')
 
               #сбор данных в таблицу
-              df[f'Распределение {teor_label}'] = df['Обеспеченность'].apply(lambda x: custom_round(selected_dist.ppf(1-x/100, *params)))
-              # Применяем форматирование
-              # df[f'Распределение {teor_label}'] = df[f'Распределение {teor_label}'].apply(custom_round)
+              df_1[f'Распределение {teor_label}'] = df['Обеспеченность'].apply(lambda x: custom_round(selected_dist.ppf(1-x/100, *params)))
+              df_2[f'Распределение {teor_label}'] = df['Обеспеченность'].apply(lambda x: custom_round(selected_dist.ppf(1-x/100, *params)))
 
             # добавление линий сетки, масштаба по горизонтальной оси, подписей осей и графика,
             # границ, шага и подписей делений для горизонтальной оси
@@ -109,24 +116,32 @@ if uploaded_file:
             st.pyplot(fig)
 
             with st.expander("# 📋 Расчет значений с разной долей обеспеченности (в %)", expanded=True):
-              #df['Обеспеченность'] = df['Обеспеченность'].astype(str) + '%'
-              #df = df.set_index("Обеспеченность")
-              df = df.T
-              #st.dataframe(df)
-              st.markdown(df.to_html(index=True, header=False), unsafe_allow_html=True)
-              #st.table(df)
+              df_1 = df.T
+              df_2 = df.T
+              st.markdown(df_1.to_html(index=True, header=False), unsafe_allow_html=True)
+              st.markdown(df_2.to_html(index=True, header=False), unsafe_allow_html=True)
 
-              # бегунок
+              # ввод значений
               p = st.number_input(
               "Выберите обеспеченность для расчета значения (0 < P < 100)",
               min_value=0.001,
               max_value=99.999,
-              value=50.0,
+              value=0,
               #step=0.01,
               #format="%.2f",  # Формат с двумя знаками после запятой
               )
-              value = selected_dist.ppf(1-p/100, *params)
-              st.markdown(f'При обеспеченности {p}% {values_col} составляет {custom_round(value)}.')
+              if p != 0:
+                custom_dict = {}
+                for disribution in distributions_to_plot:
+                  dist_key = distributions[disribution]
+                  selected_dist = getattr(stats, dist_key)
+                  params = selected_dist.fit(data[values_col])
+                  value = selected_dist.ppf(1-p/100, *params)
+                  teor_label = re.sub(r'\s*\([^)]*\)$', '', disribution)
+                  custom_dict[teor_label] = value
+                df = pd.DataFrame(list(custom_dict.items()))
+                st.markdown(df.to_html(index=False, header=False), unsafe_allow_html=True)
+                #st.markdown(f'При обеспеченности {p}% {values_col} составляет {custom_round(value)}.')
 
     except Exception as e:
         st.error(f"Ошибка: {str(e)}")
